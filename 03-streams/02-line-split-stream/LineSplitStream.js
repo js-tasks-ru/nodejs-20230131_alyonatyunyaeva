8b7ec3cd32025262d32a0c27fa2b80bb5caa353f
Alyona Tyunyaeva <alyonatyunyaeva@gmail.com>
2023-02-20 21:46:52 +0400

    add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -2,15 +2,35 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
-  }
+	constructor(options) {
+		super(options);
+		this.stringPart = '';
+	}
 
-  _transform(chunk, encoding, callback) {
-  }
+	_transform(chunk, encoding, callback) {
+		const chunkStr = chunk.toString();
+		const lines = chunkStr.split(os.EOL);
+		lines[0] = this.stringPart + lines[0];
+		if (chunkStr.endsWith(`${os.EOL}`)) {
+			this.stringPart = '';
+			lines.forEach((line) => {
+				this.push(line);
+			});
+		} else {
+			this.stringPart = lines.at(-1);
+			lines.slice(0, -1).forEach((line) => {
+				this.push(line);
+			});
+		}
+		callback();
+	}
 
-  _flush(callback) {
-  }
+	_flush(callback) {
+		if (this.stringPart) {
+			this.push(this.stringPart);
+		}
+		callback();
+	}
 }
 
 module.exports = LineSplitStream;
