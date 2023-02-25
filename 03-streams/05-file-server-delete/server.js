ddc046c95c9f82e0a1e42a320268b17a84ed744b
Alyona Tyunyaeva <alyonatyunyaeva@gmail.com>
2023-02-25 21:58:04 +0400

    add solution for 03-streams 05-file-server-delete

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,24 +1,44 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = url.pathname.slice(1);
+	const url = new URL(req.url, `http://${req.headers.host}`);
+	const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+	const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'DELETE':
+	switch (req.method) {
+		case 'DELETE':
+			if (pathname.split('/').length > 1) {
+				res.statusCode = 400;
+				res.end('wrong path');
+				return;
+			}
+			if (!fs.existsSync(filepath)) {
+				res.statusCode = 404;
+				res.end("file doesn't exists");
+				return;
+			}
+			fs.unlink(filepath, (err) => {
+				if (err) {
+					res.statusCode = 500;
+					res.end('Something went wrong');
+					return;
+				}
+				res.statusCode = 200;
+				res.end('file is deleted');
+			});
 
-      break;
+			break;
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+		default:
+			res.statusCode = 501;
+			res.end('Not implemented');
+	}
 });
 
 module.exports = server;
