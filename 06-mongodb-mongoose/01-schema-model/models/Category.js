95d0fe61a6ae2d1bd4d117ef2e3d30e6567c7a3e
Алёна Тюняева <alyonatyunyaeva@Alenas-MacBook-Pro.local>
2023-03-12 18:58:42 +0400

    add solution for 06-mongodb-mongoose 01-schema-model

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
-const subCategorySchema = new mongoose.Schema({
+const subcategorySchema = new mongoose.Schema({
+  title: { type: String, required: true },
 });
 
+const subcategory = connection.model('Subcategory', subcategorySchema);
+
 const categorySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  subcategories: [subcategorySchema],
 });
 
 module.exports = connection.model('Category', categorySchema);
